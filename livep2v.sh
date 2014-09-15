4fef2c9b0a5a130b3d5d51385bc1dc5c43e55cd6
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-09-15 16:53:00 +0200

    Some changes

--- livep2v.sh
+++ livep2v.sh
@@ -3,7 +3,7 @@
 HOST=$1
 DISK=${2:-/dev/sda}
 TARGET=${HOST}
-FMT=qcow
+FMT=raw
 
 # Cleanup old run
 sudo kpartx -d ${HOST}.${FMT} 2> /dev/null
@@ -11,36 +11,37 @@ sudo kpartx -d ${HOST}.${FMT} 2> /dev/null
 if [ ! -f ${HOST}.${FMT} ]; then
   # Create disk image
   disk_size=$(ssh root@${HOST} fdisk -l ${DISK} 2> /dev/null | grep "Disk ${DISK}"|cut -f5 -d' ')
-  $DEBUG qemu-img create ${HOST}.${FMT} ${disk_size} > /dev/null
+  $DEBUG qemu-img create ${HOST}.${FMT} ${disk_size} > /dev/null || exit 1
 
   # Dump and import Partition table
-  ssh root@${HOST} "sfdisk -d ${DISK}" 2> /dev/null | /sbin/sfdisk ${HOST}.${FMT} > /dev/null 2>&1
+  ssh root@${HOST} "dd if=${DISK} bs=512 count=1" | dd of=${HOST}.${FMT} bs=512 count=1 conv=notrunc || exit 1
+  ssh root@${HOST} "sfdisk -d ${DISK}" 2> /dev/null | /sbin/sfdisk ${HOST}.${FMT} > /dev/null 2>&1 || exit 1
 
   # Create partition mapping
-  $DEBUG sudo kpartx -as ${HOST}.${FMT}
+  $DEBUG sudo kpartx -as ${HOST}.${FMT} || exit 1
 
   # Create filesystem
-  mkdir -p ${TARGET}
+  mkdir -p ${TARGET} || exit 1
   ssh root@${HOST} "mount|grep ^${DISK}" | while read device on mount_point type fstype options; do
     device_number=$(echo ${device} | sed "s:${DISK}::")
-    $DEBUG sudo mkfs.${fstype} -F /dev/mapper/loop0p${device_number}
+    $DEBUG sudo mkfs.${fstype} -F /dev/mapper/loop0p${device_number} || exit 1
   done
 
   # Create swap
   for device in $(ssh root@${HOST} "swapon -s|grep ^${DISK}|cut -f1 -d' '"); do
     device_number=$(echo ${device} | sed "s:${DISK}::")
-    $DEBUG sudo mkswap -f /dev/mapper/loop0p${device_number} > /dev/null
+    $DEBUG sudo mkswap -f /dev/mapper/loop0p${device_number} > /dev/null || exit 1
   done
 else
   # Create partition mapping
-  $DEBUG sudo kpartx -as ${HOST}.${FMT}
+  $DEBUG sudo kpartx -as ${HOST}.${FMT} || exit 1
 fi
 
 # Mount filesystems
 ssh root@${HOST} "mount|grep ^${DISK}" | while read device on mount_point type fstype options; do
   device_number=$(echo ${device} | sed "s:${DISK}::")
-  $DEBUG sudo mkdir -p ${TARGET}${mount_point}
-  $DEBUG sudo mount -t${fstype} /dev/mapper/loop0p${device_number} ${TARGET}${mount_point}
+  $DEBUG sudo mkdir -p ${TARGET}${mount_point} || exit 1
+  $DEBUG sudo mount -t${fstype} /dev/mapper/loop0p${device_number} ${TARGET}${mount_point} || exit 1
 done
 
 # Sync files
