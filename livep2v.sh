3d531d2fd20b308299ea8a3645dd946bd2671aba
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-09-29 13:39:15 +0200

    Add support for LVM

--- livep2v.sh
+++ livep2v.sh
@@ -22,26 +22,53 @@ if [ ! -f ${HOST}.${FMT} ]; then
   # Create partition mapping
   $DEBUG sudo kpartx -as ${HOST}.${FMT} || exit 1
 
-  # Create filesystem
-  mkdir -p ${TARGET} || exit 1
-  ssh root@${HOST} "blkid|grep ^${DISK}|grep -v -E 'TYPE=\"(swap|vfat)\"'" | while read device vars; do
+  # Create LVM2 volume
+  ssh root@${HOST} "blkid -t TYPE=LVM2_member" | while read device vars; do
     device=${device%?}
     device_number=$(echo ${device} | sed "s:${DISK}::")
+    ssh -n root@${HOST} "dd if=${device} bs=512 count=24" | $DEBUG sudo dd of=/dev/mapper/loop0p${device_number} bs=512 count=24 conv=notrunc || exit 1
+    $DEBUG sudo pvs
+    $DEBUG sudo lvm vgchange -ay
+    ls /dev/mapper
+    sleep 3
+  done
+
+  # Create filesystem
+  ssh root@${HOST} "blkid|grep -v -E 'TYPE=\"(swap|vfat|LVM2_member)\"'" | while read device vars; do
+    device=${device%?}
     eval $vars
-    $DEBUG sudo mkfs.${TYPE} -U ${UUID} /dev/mapper/loop0p${device_number} || exit 1
+    if [[ $device =~ $DISK ]]; then
+      device_number=$(echo ${device} | sed "s:${DISK}::")
+      $DEBUG sudo mkfs.${TYPE} -U ${UUID} /dev/mapper/loop0p${device_number} || exit 1
+    elif [[ $device =~ /dev/mapper ]]; then
+      $DEBUG sudo mkfs.${TYPE} -U ${UUID} "${device}" || exit 1
+    fi
   done
 
   # Create swap
-  for device in $(ssh root@${HOST} "swapon -s|grep ^${DISK}|cut -f1 -d' '"); do
-    device_number=$(echo ${device} | sed "s:${DISK}::")
-    $DEBUG sudo mkswap -f /dev/mapper/loop0p${device_number} > /dev/null || exit 1
+  for device in $(ssh root@${HOST} "swapon -s|tail -n +2|cut -f1 -d' '"); do
+    if [[ $device =~ $DISK ]]; then
+      device_number=$(echo ${device} | sed "s:${DISK}::")
+      $DEBUG sudo mkswap -f /dev/mapper/loop0p${device_number} || exit 1
+    elif [[ $device =~ /dev/mapper ]]; then
+      $DEBUG sudo mkswap -f ${device} || exit 1
+    fi
   done
 else
   # Create partition mapping
   $DEBUG sudo kpartx -as ${HOST}.${FMT} || exit 1
+  $DEBUG sudo pvs
+  $DEBUG sudo lvm vgchange -ay
 fi
 
 # Mount filesystems
+$DEBUG mkdir -p ${TARGET} || exit 1
+
+ssh root@${HOST} "mount|grep ^/dev/mapper" | while read device on mount_point type fstype options; do
+  $DEBUG sudo mkdir -p ${TARGET}${mount_point} || exit 1
+  $DEBUG sudo mount -t${fstype} ${device} ${TARGET}${mount_point} || exit 1
+done
+
 ssh root@${HOST} "mount|grep ^${DISK}" | while read device on mount_point type fstype options; do
   device_number=$(echo ${device} | sed "s:${DISK}::")
   $DEBUG sudo mkdir -p ${TARGET}${mount_point} || exit 1
@@ -49,14 +76,15 @@ ssh root@${HOST} "mount|grep ^${DISK}" | while read device on mount_point type f
 done
 
 # Sync files
-$DEBUG sudo rsync -aAXvP root@${HOST}:/ ${TARGET}/ --delete --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs}
-$DEBUG sync
+$DEBUG sudo rsync -aAXvP root@${HOST}:/ ${TARGET}/ --delete --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs,/home/*,/var/lib/glance/*}
 
 # Unmount filesystem
 ssh root@${HOST} "mount|grep ^${DISK}|tac" | while read device on mount_point type fstype options; do
   device_number=$(echo ${device} | sed "s:${DISK}::")
   $DEBUG sudo umount /dev/mapper/loop0p${device_number} || exit 1
 done
+
+$DEBUG sudo lvm vgchange -an
 
 # Delete partition mapping
 $DEBUG sudo kpartx -d ${HOST}.${FMT}
