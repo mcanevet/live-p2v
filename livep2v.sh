6bc3b2d7def6ccecfb7a7616ca0869a6ada72515
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-09-29 15:38:29 +0200

    Use qemu-nbd instead of kpartx

--- livep2v.sh
+++ livep2v.sh
@@ -6,7 +6,7 @@ TARGET=${HOST}
 FMT=raw
 
 # Cleanup old run
-sudo kpartx -d ${HOST}.${FMT} 2> /dev/null
+sudo qemu-nbd -d /dev/nbd0
 
 if [ ! -f ${HOST}.${FMT} ]; then
   # Create disk image
@@ -20,13 +20,13 @@ if [ ! -f ${HOST}.${FMT} ]; then
   ssh root@${HOST} "sfdisk -d ${DISK}" 2> /dev/null | /sbin/sfdisk ${HOST}.${FMT} > /dev/null 2>&1 || exit 1
 
   # Create partition mapping
-  $DEBUG sudo kpartx -as ${HOST}.${FMT} || exit 1
+  $DEBUG sudo qemu-nbd -c /dev/nbd0 ${HOST}.${FMT} || exit 1
 
   # Create LVM2 volume
   ssh root@${HOST} "blkid -t TYPE=LVM2_member" | while read device vars; do
     device=${device%?}
     device_number=$(echo ${device} | sed "s:${DISK}::")
-    ssh -n root@${HOST} "dd if=${device} bs=512 count=24" | $DEBUG sudo dd of=/dev/mapper/loop0p${device_number} bs=512 count=24 conv=notrunc || exit 1
+    ssh -n root@${HOST} "dd if=${device} bs=512 count=24" | $DEBUG sudo dd of=/dev/nbd0p${device_number} bs=512 count=24 conv=notrunc || exit 1
     $DEBUG sudo pvs
     $DEBUG sudo lvm vgchange -ay
     ls /dev/mapper
@@ -39,7 +39,7 @@ if [ ! -f ${HOST}.${FMT} ]; then
     eval $vars
     if [[ $device =~ $DISK ]]; then
       device_number=$(echo ${device} | sed "s:${DISK}::")
-      $DEBUG sudo mkfs.${TYPE} -U ${UUID} /dev/mapper/loop0p${device_number} || exit 1
+      $DEBUG sudo mkfs.${TYPE} -U ${UUID} /dev/nbd0p${device_number} || exit 1
     elif [[ $device =~ /dev/mapper ]]; then
       $DEBUG sudo mkfs.${TYPE} -U ${UUID} "${device}" || exit 1
     fi
@@ -49,14 +49,14 @@ if [ ! -f ${HOST}.${FMT} ]; then
   for device in $(ssh root@${HOST} "swapon -s|tail -n +2|cut -f1 -d' '"); do
     if [[ $device =~ $DISK ]]; then
       device_number=$(echo ${device} | sed "s:${DISK}::")
-      $DEBUG sudo mkswap -f /dev/mapper/loop0p${device_number} || exit 1
+      $DEBUG sudo mkswap -f /dev/nbd0p${device_number} || exit 1
     elif [[ $device =~ /dev/mapper ]]; then
       $DEBUG sudo mkswap -f ${device} || exit 1
     fi
   done
 else
   # Create partition mapping
-  $DEBUG sudo kpartx -as ${HOST}.${FMT} || exit 1
+  $DEBUG sudo qemu-nbd -c /dev/nbd0 ${HOST}.${FMT} || exit 1
   $DEBUG sudo pvs
   $DEBUG sudo lvm vgchange -ay
 fi
@@ -72,7 +72,7 @@ done
 ssh root@${HOST} "mount|grep ^${DISK}" | while read device on mount_point type fstype options; do
   device_number=$(echo ${device} | sed "s:${DISK}::")
   $DEBUG sudo mkdir -p ${TARGET}${mount_point} || exit 1
-  $DEBUG sudo mount -t${fstype} /dev/mapper/loop0p${device_number} ${TARGET}${mount_point} || exit 1
+  $DEBUG sudo mount -t${fstype} /dev/nbd0p${device_number} ${TARGET}${mount_point} || exit 1
 done
 
 # Sync files
@@ -81,10 +81,10 @@ $DEBUG sudo rsync -aAXvP root@${HOST}:/ ${TARGET}/ --delete --exclude={/dev/*,/p
 # Unmount filesystem
 ssh root@${HOST} "mount|grep ^${DISK}|tac" | while read device on mount_point type fstype options; do
   device_number=$(echo ${device} | sed "s:${DISK}::")
-  $DEBUG sudo umount /dev/mapper/loop0p${device_number} || exit 1
+  $DEBUG sudo umount /dev/nbd0p${device_number} || exit 1
 done
 
 $DEBUG sudo lvm vgchange -an
 
 # Delete partition mapping
-$DEBUG sudo kpartx -d ${HOST}.${FMT}
+$DEBUG sudo qemu-nbd -d /dev/nbd0
